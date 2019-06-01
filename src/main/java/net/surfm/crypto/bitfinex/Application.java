e5b8cf0af44d00679b33538d85d43ed6709ec505
kirin <kirin.chen1001@gmail.com>
2019-06-01 12:21:12 +0800

    setup test spring boot framework

--- src/main/java/net/surfm/crypto/bitfinex/Application.java
+++ src/main/java/net/surfm/crypto/bitfinex/Application.java
@@ -1,12 +1,22 @@
 package net.surfm.crypto.bitfinex;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.web.client.RestTemplate;
 
+@Configuration
+@EnableAutoConfiguration
+@EnableScheduling
+@ComponentScan(basePackages = { "net.surfm" })
 @SpringBootApplication
+@PropertySource({ "classpath:prod.properties", "classpath:${env}.properties" })
 public class Application {
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
---
+++ src/test/java/net/surfm/crypto/bitfinex/api/TestApiClient.java
@@ -0,0 +1,11 @@
+package net.surfm.crypto.bitfinex.api;
+
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class TestApiClient {
+
+}
